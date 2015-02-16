028335350edc2f6131dfdd8250cb783ab64c56cb
mileslux <mileslux@gmail.com>
2015-02-16 04:07:42 +0300

    Added the first test

--- src/main/java/com/infra/common/Main.java
+++ src/main/java/com/infra/common/Main.java
@@ -1,4 +1,4 @@
-package infra;
+package com.infra.common;
 
 import org.apache.commons.configuration.*;
 
---
+++ src/main/java/com/infra/common/Woofer.java
@@ -0,0 +1,10 @@
+package com.infra.common;
+
+/**
+ * Created by mileslux on 2/16/15.
+ */
+public class Woofer {
+    public static String woof() {
+        return "Woof";
+    }
+}
